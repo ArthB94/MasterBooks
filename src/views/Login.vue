f3c500a139d78793a5667ee6c018bdf39afb6a5f
ArthB94 <80051198+ArthB94@users.noreply.github.com>
2023-06-16 13:31:52 +0200

    installation axios

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,47 +1,8 @@
 <template>
-  <!-- <?php
-    //start a session
-    session_start();
-        include 'dbConn.php';
-        if(isset($_POST['login'])){
-            $email=$_POST['txtEmail'];
-            $password=md5($_POST['txtPassword']);
-            $query="SELECT * FROM tblusers WHERE email='$email' AND password='$password'";
-            $result=mysqli_query($connection,$query);
-            $row = mysqli_fetch_assoc($result); //$row['email']
-            $count = mysqli_num_rows($result); //1 or 0
-            if($count == 1){
-                //record found
-                echo 'login successful';
-                $_SESSION['fullname'] = $row['firstname'];
-                $_SESSION['email'] = $row['email'];
-                $_SESSION['uid'] = $row['ID'];
-                $_SESSION['admin'] = $row['admin'];
-                echo '<hr>
-                    ';
-                                echo 'Full name :' . $_SESSION['fullname'] . '<br>';
-                                echo 'Email : ' . $_SESSION['email'] . '<br>';
-                                echo 'ID : ' . $_SESSION['uid'] . '<br>';
-                                //redirect the user to the main page
-                                header("Location: index.php");
-                            }else{
-                                echo 'invalid credentials';
-                            }
-                        }
-                    mysqli_close($connection);
-                    ?> -->
 
   <body>
     <div class="LoginPage">
-      <!-- <div class="loginColor"></div>
-                            <div class="loginColor"></div>
-                            <div class="loginColor"></div> -->
       <div class="loginBox">
-        <!-- <div class="loginSquares" style="--i:0;"></div>
-                                <div class="loginSquares" style="--i:1;"></div>
-                                <div class="loginSquares" style="--i:2;"></div>
-                                <div class="loginSquares" style="--i:3;"></div>
-                                <div class="loginSquares" style="--i:4;"></div> -->
         <div class="loginContainer">
           <div class="loginForm">
             <h2>Welcome back</h2>
@@ -154,19 +115,6 @@ export default {
   },
 };
 
-//   data() {
-//     return {
-//       open: true,
-//     }
-//   },
-//   methods: {
-//     openModal() {
-//       this.open = true
-//     },
-//     closeModal() {
-//       this.open = false
-//     },
-//   },
 </script>
 
 <style></style>

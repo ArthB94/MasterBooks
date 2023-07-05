218c619d3e5b8fb1329bde98f2e80bce8a36167b
Arthur <arthur.billebaut@efrei.net>
2023-07-05 10:10:15 +0000

    Bug si myLibrary vide recommendations

--- server/app/config/main.py
+++ server/app/config/main.py
@@ -68,200 +68,202 @@ def get_references_lecture(dataframe):
     references = dataframe.loc[dataframe['lecture'] == 1, 'reference'].tolist()
     return references
 livres_lus = get_references_lecture(dataframe)
+print (len(livres_lus))
+if (len(livres_lus)>1):
+        
+    # ------------------------------ Pre-processing sur le dataframe ------------------------------
 
 
-# ------------------------------ Pre-processing sur le dataframe ------------------------------
+
+    # supprimer les lignes du dataframe qui contiennent au moins une valeur manquante donc un NaN
+    dataframe = dataframe.dropna(how="any")
+
+    langues_a_supprimer = ['others'] 
+    dataframe = dataframe.drop(dataframe[dataframe['langue'].isin(langues_a_supprimer)].index)
 
 
+    df = dataframe
 
-# supprimer les lignes du dataframe qui contiennent au moins une valeur manquante donc un NaN
-dataframe = dataframe.dropna(how="any")
+    print(df)
 
-langues_a_supprimer = ['others'] 
-dataframe = dataframe.drop(dataframe[dataframe['langue'].isin(langues_a_supprimer)].index)
-
-
-df = dataframe
-
-print(df)
-
-# Définition des caractères indésirables
-carac_indesirables = string.punctuation + string.digits # retourne cela : !"#$%&'()*+,-./:;<=>?@[\]^_`{|}~0123456789
+    # Définition des caractères indésirables
+    carac_indesirables = string.punctuation + string.digits # retourne cela : !"#$%&'()*+,-./:;<=>?@[\]^_`{|}~0123456789
 
 
-# cette fonction permet de mettre un espace aprè un point car il n'y en a pas toujours et parfois ça pose problème
-def espaceApresPoint(text):
-    updated_text = ""
-    for i in range(len(text)):
-        if text[i] == "." and i < len(text) - 1 and text[i+1] != " ":
-            updated_text += ". "
+    # cette fonction permet de mettre un espace aprè un point car il n'y en a pas toujours et parfois ça pose problème
+    def espaceApresPoint(text):
+        updated_text = ""
+        for i in range(len(text)):
+            if text[i] == "." and i < len(text) - 1 and text[i+1] != " ":
+                updated_text += ". "
+            else:
+                updated_text += text[i]
+        return updated_text
+
+
+    # Initialisation du lemmatiseur
+    lemmatizer = WordNetLemmatizer() # retourne cela : <WordNetLemmatizer>
+
+    def preProcessing_description(ligne):
+
+        text = ligne['resume']
+        language = ligne['langue']
+
+        text = espaceApresPoint(text)
+
+        text = ''.join(char if char not in carac_indesirables else ' ' for char in text)
+
+        # on met tout en minuscule
+        text = text.lower()
+
+        # tokenisation du texte, ex: on passe de "Je vais au supermarché" à '['Je', 'vais', 'au', 'supermarché']'
+        tokens = word_tokenize(text)
+
+        # définition des mots vides 
+        if language == 'French' : 
+            stop_words = set(stopwords.words('french'))
+        elif language == 'English':
+            stop_words = set(stopwords.words('english'))
+        elif language =='German' :
+            stop_words = set(stopwords.words('german'))
         else:
-            updated_text += text[i]
-    return updated_text
+            stop_words = set(stopwords.words('spanish'))
 
+        tokens = [token for token in tokens if token not in stop_words]
 
-# Initialisation du lemmatiseur
-lemmatizer = WordNetLemmatizer() # retourne cela : <WordNetLemmatizer>
+        tokens = [lemmatizer.lemmatize(token) for token in tokens]
 
-def preProcessing_description(ligne):
+        preprocessed_text = ' '.join(tokens)
 
-    text = ligne['resume']
-    language = ligne['langue']
-
-    text = espaceApresPoint(text)
-
-    text = ''.join(char if char not in carac_indesirables else ' ' for char in text)
-
-    # on met tout en minuscule
-    text = text.lower()
-
-    # tokenisation du texte, ex: on passe de "Je vais au supermarché" à '['Je', 'vais', 'au', 'supermarché']'
-    tokens = word_tokenize(text)
-
-    # définition des mots vides 
-    if language == 'French' : 
-        stop_words = set(stopwords.words('french'))
-    elif language == 'English':
-        stop_words = set(stopwords.words('english'))
-    elif language =='German' :
-        stop_words = set(stopwords.words('german'))
-    else:
-        stop_words = set(stopwords.words('spanish'))
-
-    tokens = [token for token in tokens if token not in stop_words]
-
-    tokens = [lemmatizer.lemmatize(token) for token in tokens]
-
-    preprocessed_text = ' '.join(tokens)
-
-    return preprocessed_text
+        return preprocessed_text
 
 
 
-df['resume'] = df.apply(preProcessing_description, axis=1) # rappel : axis = 1  première ligne
+    df['resume'] = df.apply(preProcessing_description, axis=1) # rappel : axis = 1  première ligne
 
-# --------------- Vectorisation de la colonne description -------------------
+    # --------------- Vectorisation de la colonne description -------------------
 
-vectorizer = TfidfVectorizer()# TfidfVectorizer(max_features=1000, min_df=3, max_df=0.85)
-descriptions_vectorized = vectorizer.fit_transform(df['resume'])
-features = vectorizer.get_feature_names()
-
-
-k = 5
-kmeans = KMeans(n_clusters=k, random_state=42, n_init=10)
-clusters = kmeans.fit_predict(descriptions_vectorized)
-df['cluster'] = clusters
+    vectorizer = TfidfVectorizer()# TfidfVectorizer(max_features=1000, min_df=3, max_df=0.85)
+    descriptions_vectorized = vectorizer.fit_transform(df['resume'])
+    features = vectorizer.get_feature_names()
 
 
-# Obtenir les centres de cluster
-centers = kmeans.cluster_centers_
+    k = 5
+    kmeans = KMeans(n_clusters=k, random_state=42, n_init=10)
+    clusters = kmeans.fit_predict(descriptions_vectorized)
+    df['cluster'] = clusters
 
-num_words = 20
-top_words = []
-for center in centers:
-    # Obtenir les indices des mots les plus importants (ceux avec les valeurs les plus élevées)
-    top_word_indices = center.argsort()[-num_words:][::-1]
-    # Obtenir les mots correspondants à ces indices
-    top_words.append([vectorizer.get_feature_names()[i] for i in top_word_indices])
 
-# Afficher les mots dans chaque cluster
-for i, words in enumerate(top_words):
-    print(f"Cluster {i+1}: {', '.join(words)}")
+    # Obtenir les centres de cluster
+    centers = kmeans.cluster_centers_
+
+    num_words = 20
+    top_words = []
+    for center in centers:
+        # Obtenir les indices des mots les plus importants (ceux avec les valeurs les plus élevées)
+        top_word_indices = center.argsort()[-num_words:][::-1]
+        # Obtenir les mots correspondants à ces indices
+        top_words.append([vectorizer.get_feature_names()[i] for i in top_word_indices])
+
+    # Afficher les mots dans chaque cluster
+    for i, words in enumerate(top_words):
+        print(f"Cluster {i+1}: {', '.join(words)}")
 
 
 
 
-def analyse_cluster(int): # int est un entier entre 1 et 1000 (les lignes)
-    cluster_1_books = df[df['cluster'] == int]
-    cluster_1_keywords = ' '.join(cluster_1_books['resume']).split()
-    keyword_counts = Counter(cluster_1_keywords)
-    top_keywords = keyword_counts.most_common(10)
-    print(top_keywords)
-    top_authors = cluster_1_books['auteur'].value_counts().head(10)
-    print(top_authors)
-    top_genres = cluster_1_books['genres'].value_counts().head(10)
-    print(top_genres)
+    def analyse_cluster(int): # int est un entier entre 1 et 1000 (les lignes)
+        cluster_1_books = df[df['cluster'] == int]
+        cluster_1_keywords = ' '.join(cluster_1_books['resume']).split()
+        keyword_counts = Counter(cluster_1_keywords)
+        top_keywords = keyword_counts.most_common(10)
+        print(top_keywords)
+        top_authors = cluster_1_books['auteur'].value_counts().head(10)
+        print(top_authors)
+        top_genres = cluster_1_books['genres'].value_counts().head(10)
+        print(top_genres)
 
 
-inertia = kmeans.inertia_
-print("Inertie :", inertia)
-
-
-
-labels = kmeans.labels_
-silhouette_avg = silhouette_score(descriptions_vectorized, labels)
-print("Silhouette moyenne :", silhouette_avg)
+    inertia = kmeans.inertia_
+    print("Inertie :", inertia)
 
 
 
-def trouver_livres_similaires(liste_livres):
-    # Trouver les clusters correspondants aux livres de la liste donnée
-    clusters = df.loc[df['reference'].isin(liste_livres), 'cluster'].values
-    
-    # Récupérer tous les autres livres des clusters correspondants où 'lire' vaut False
-    similar_books = df.loc[(df['cluster'].isin(clusters)) & (~df['reference'].isin(liste_livres)) & (df['lecture'] == 0), 'reference'].values
-    
-    # Obtenir les descriptions des livres de la liste donnée
-    descriptions_liste_livres = descriptions_vectorized[df['reference'].isin(liste_livres)]
-    
-    # Obtenir les descriptions des livres similaires
-    descriptions_similar_books = descriptions_vectorized[df['reference'].isin(similar_books)]
-    
-    # Calculer les distances entre les livres recommandés et la liste de livres donnée
-    distances = pairwise_distances(descriptions_similar_books, descriptions_liste_livres)
-    
-    # Convertir les distances en pourcentage
-    max_distance = distances.max()
-    similarity_percentages = 100 - (distances / max_distance) * 100
-    similarity_percentages = np.round(100 - similarity_percentages,0)
-    
-    # Créer un DataFrame contenant les livres recommandés et les pourcentages
-    result_df = pd.DataFrame({'reference': similar_books, 'Pourcentage': similarity_percentages.max(axis=1)})
-
-    # Ajouter la colonne email_user avec l'email spécifié
-    result_df = result_df.assign(email_user = email_user)
-    
-    # Trier le DataFrame par ordre décroissant selon la colonne "pourcentage"
-    result_df = result_df.sort_values(by='Pourcentage', ascending=True)
-    
-    # Sélectionner les trois premières lignes
-    result_df = result_df.head(6)
-    
-    # Réinitialiser les index du DataFrame
-    result_df = result_df.reset_index(drop=True)
-    
-    # Retourner le DataFrame des livres recommandés avec les trois plus hauts pourcentages
-    return result_df
+    labels = kmeans.labels_
+    silhouette_avg = silhouette_score(descriptions_vectorized, labels)
+    print("Silhouette moyenne :", silhouette_avg)
 
 
 
-resultat = trouver_livres_similaires(livres_lus)
-print(resultat)
+    def trouver_livres_similaires(liste_livres):
+        # Trouver les clusters correspondants aux livres de la liste donnée
+        clusters = df.loc[df['reference'].isin(liste_livres), 'cluster'].values
+        
+        # Récupérer tous les autres livres des clusters correspondants où 'lire' vaut False
+        similar_books = df.loc[(df['cluster'].isin(clusters)) & (~df['reference'].isin(liste_livres)) & (df['lecture'] == 0), 'reference'].values
+        
+        # Obtenir les descriptions des livres de la liste donnée
+        descriptions_liste_livres = descriptions_vectorized[df['reference'].isin(liste_livres)]
+        
+        # Obtenir les descriptions des livres similaires
+        descriptions_similar_books = descriptions_vectorized[df['reference'].isin(similar_books)]
+        
+        # Calculer les distances entre les livres recommandés et la liste de livres donnée
+        distances = pairwise_distances(descriptions_similar_books, descriptions_liste_livres)
+        
+        # Convertir les distances en pourcentage
+        max_distance = distances.max()
+        similarity_percentages = 100 - (distances / max_distance) * 100
+        similarity_percentages = np.round(100 - similarity_percentages,0)
+        
+        # Créer un DataFrame contenant les livres recommandés et les pourcentages
+        result_df = pd.DataFrame({'reference': similar_books, 'Pourcentage': similarity_percentages.max(axis=1)})
 
-# supprimer tout le contenue de être_recommandé à chaque fois
-delete_query = "DELETE FROM être_recommandé WHERE email_user = '"+email_user+"'"
+        # Ajouter la colonne email_user avec l'email spécifié
+        result_df = result_df.assign(email_user = email_user)
+        
+        # Trier le DataFrame par ordre décroissant selon la colonne "pourcentage"
+        result_df = result_df.sort_values(by='Pourcentage', ascending=True)
+        
+        # Sélectionner les trois premières lignes
+        result_df = result_df.head(6)
+        
+        # Réinitialiser les index du DataFrame
+        result_df = result_df.reset_index(drop=True)
+        
+        # Retourner le DataFrame des livres recommandés avec les trois plus hauts pourcentages
+        return result_df
 
-# Exécuter la requête DELETE
-cursor.execute(delete_query)
 
-# Valider la suppression
-connection.commit()
 
-# Parcourir les lignes du DataFrame et insérer chaque ligne dans la table être_recommandé
-for index, row in resultat.iterrows():
-    email_user = email_user  # Récupérer la valeur de email_user (remplacer par votre code)
-    reference = row['reference']
-    pourcentage = row['Pourcentage']
-    
-    # Requête SQL d'insertion
-    insert_query = "INSERT INTO être_recommandé (email_user, reference, Pourcentage) VALUES (%s, %s, %s)"
-    
-    # Exécuter la requête d'insertion avec les valeurs correspondantes
-    cursor.execute(insert_query, (email_user, reference, pourcentage))
+    resultat = trouver_livres_similaires(livres_lus)
+    print(resultat)
 
-# Valider les insertions
-connection.commit()
+    # supprimer tout le contenue de être_recommandé à chaque fois
+    delete_query = "DELETE FROM être_recommandé WHERE email_user = '"+email_user+"'"
 
+    # Exécuter la requête DELETE
+    cursor.execute(delete_query)
+
+    # Valider la suppression
+    connection.commit()
+
+    # Parcourir les lignes du DataFrame et insérer chaque ligne dans la table être_recommandé
+    for index, row in resultat.iterrows():
+        email_user = email_user  # Récupérer la valeur de email_user (remplacer par votre code)
+        reference = row['reference']
+        pourcentage = row['Pourcentage']
+        
+        # Requête SQL d'insertion
+        insert_query = "INSERT INTO être_recommandé (email_user, reference, Pourcentage) VALUES (%s, %s, %s)"
+        
+        # Exécuter la requête d'insertion avec les valeurs correspondantes
+        cursor.execute(insert_query, (email_user, reference, pourcentage))
+
+    # Valider les insertions
+    connection.commit()
+else :
+    print ("L'utilisateur n'a pas lu de livre")
 # Fermer le curseur et la connexion
 cursor.close()
 connection.close()
